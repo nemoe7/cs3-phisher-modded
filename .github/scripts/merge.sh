782e7d237a67ce585a09d156756412df49af92ea
stanpai <stanpai78@gmail.com>
2025-05-21 10:58:55 +0800

    ci(sync)!: update merge.sh, syncupstream.yml

--- .github/scripts/merge.sh
+++ .github/scripts/merge.sh
@@ -1,13 +1,4 @@
-git remote add upstream https://github.com/phisher98/cloudstream-extensions-phisher.git || true
-git fetch upstream
-
-# Check if upstream is ahead
-UPSTREAM_HASH=$(git rev-parse upstream/master)
-ORIGIN_HASH=$(git rev-parse origin/master)
-if [ "$UPSTREAM_HASH" = "$ORIGIN_HASH" ]; then
-  echo "Upstream is not ahead. Exiting."
-  exit 78
-fi
+# /.github/scripts/merge.sh
 
 # Get directories deleted in fork
 git diff --name-only --diff-filter=D 0a82eb78dab7b698939754d21c5adf3bcc841692 > delete
